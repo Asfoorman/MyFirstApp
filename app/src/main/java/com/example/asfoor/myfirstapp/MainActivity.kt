10da555b4ed447bae9a0a37078458460bf2588ef
Anes Mehai <nsamehai@gmail.com>
2018-06-06 14:35:56 -0400

    Adding Android App from 6-6-2018

--- app/src/main/java/com/example/asfoor/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/asfoor/myfirstapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.asfoor.myfirstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun changeText(){
+        btnSayHello.setOnClickListener{
+            lblText.setText("HI ANES!!")
+        }
     }
 }
